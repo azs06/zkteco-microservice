7a40245b19565fb8af384893e9eccd665b08cdc9
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-29 15:51:39 +0600

    more experiment

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const test = async () => {
   try {
     // Create socket to machine
     await zkInstance.createSocket();
-
     console.log("Socket created");
 
     // Get general info like logCapacity, user counts, logs count
@@ -18,31 +17,13 @@ const test = async () => {
     }
   }
 
-  // Get all logs in the machine
-  // Currently, there is no filter to take data, it just takes all !!
-  const logs = await zkInstance.getAttendances();
-
-  const attendances = await zkInstance.getAttendances((percent, total) => {
-    // this callbacks take params is the percent of data downloaded and total data need to download
-  });
-
-  // console.log('check users', users)
-  await zkInstance.enableDevice(); 
+  await zkInstance.enableDevice();
   await zkInstance.getRealTimeLogs((data) => {
+    console.log("logging")
     // do something when some checkin
     console.log(data);
   });
 
-  // delete the data in machine
-  // You should do this when there are too many data in the machine, this issue can slow down machine
-  //zkInstance.clearAttendanceLog();
-
-  // Get the device time
-  //const getTime = await zkInstance.getTime();
-  //console.log(getTime.toString());
-
-  // Disconnect the machine ( don't do this when you need realtime update :)))
-  //await zkInstance.disconnect();
 };
 
 test();
--- server.js
+++ server.js
@@ -1,20 +1,60 @@
 const net = require('net');
 const fs = require('fs');
 
-// Create a TCP server
 const server = net.createServer((socket) => {
   console.log('Device connected:', socket.remoteAddress);
 
+  // Accumulate data across chunks (in case of fragmented messages)
+  let buffer = '';
+
   socket.on('data', (data) => {
-    // Parse the raw data (example: ZKTeco's protocol may use hex/binary)
-    console.log("Data recieved");
-    console.log({ data: data.toString() });
+    buffer += data.toString(); // Append incoming data to buffer
+
+    // Split messages by line (ZKTeco often uses \r\n as a delimiter)
+    const messages = buffer.split('\r\n');
+    buffer = messages.pop(); // Leave incomplete messages in the buffer
+
+    messages.forEach((message) => {
+      if (message.trim() === '') return; // Skip empty lines
+
+      // Parse the message into key-value pairs
+      const parsedData = parseZKTecoData(message);
+      console.log('Parsed Data:', parsedData);
+
+      // Example: Log to a file/database
+      if (parsedData.PIN) {
+        fs.appendFileSync('attendance.log', `User ${parsedData.PIN} scanned at ${parsedData.DateTime}\n`);
+      }
+    });
   });
 
   socket.on('end', () => {
     console.log('Device disconnected');
   });
 });
+
+// Parse ZKTeco's key-value format (e.g., "PIN=5\tDateTime=...")
+function parseZKTecoData(rawData) {
+  if (rawData.startsWith('GET /iclock/cdata')) {
+    // Parse the initial handshake (query parameters)
+    const url = new URL(`http://dummy${rawData.split(' ')[1]}`);
+    const sn = url.searchParams.get('SN');
+    console.log('Device SN:', sn);
+    return { type: 'handshake', SN: sn };
+  } else if (rawData.includes('=')) {
+    // Parse key-value attendance data
+    const entries = rawData.split('\t');
+    const parsed = {};
+    entries.forEach((entry) => {
+      const [key, value] = entry.split('=');
+      if (key && value) parsed[key] = value;
+    });
+    return { type: 'attendance', ...parsed };
+  } else {
+    // Unknown message type (e.g., heartbeat)
+    return { type: 'unknown', rawData };
+  }
+}
 
 server.listen(8081, '0.0.0.0', () => {
   console.log('TCP server listening on port 8081');
